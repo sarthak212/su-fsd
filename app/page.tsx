f5650d1586f74fcc10ac8a58d890d8b309e57146
Sarthak Kaushik <skkaushik212@gmail.com>
2024-10-15 14:16:53 +0530

    Env changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import { Box, Container } from "@radix-ui/themes";
 export default async function Home() {
   let formatData = [];
   try {
-    const data = await fetch(`${process.env.HOST_URL}/api/lists`);
+    let url = process.env.HOST_URL ? `http://${process.env.HOST_URL}/api/lists` : null;
+    if (!url) {
+      url = `https://${process.env.VERCEL_URL}/api/lists`;
+    }
+    const data = await fetch(url);
     const receivedData = await data.json();
     formatData = receivedData.map((record: { date: string; name: string }) => {
       return {
